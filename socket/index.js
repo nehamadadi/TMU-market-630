eea2dcc928694233acd2e053e563abec7575b89c
Alexia-Ward <128848894+Alexia-Ward@users.noreply.github.com>
2024-04-06 02:37:23 -0400

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -1,42 +1,45 @@
-//Setup the messaging socket
-const io = require("socket.io")(3002, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
+const io = require('socket.io')(process.env.SOCKET_IO_PORT || 3002, {
+    cors: {
+        origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    }
 });
 
 // Holds online users
 let activeUsers = [];
 
+//Handles app messages 
 io.on("connection", (socket) => {
-  // new-user-add: add user onto the socket and log them as active
-  socket.on("new-user-add", (newUserId) => {
-    if (!activeUsers.some((user) => user.userId === newUserId)) {
-      activeUsers.push({
-        userId: newUserId,
-        socketId: socket.id,
-      });
-    }
-    //send users to clientside
-    io.emit("get-users", activeUsers);
-  });
+    
+    // Add new user, add user onto the socket
+    socket.on("new-user-add", (newUserId) => {
+        if (!activeUsers.some((user) => user.userId === newUserId)) {
+            activeUsers.push({
+                userId: newUserId,
+                socketId: socket.id
+            });
+        }
+        // Send users to clientside 
+        console.log("Connected Users", activeUsers);
+        io.emit('get-users', activeUsers);
+    });
 
-  //disconnect: remove user from the active users
-  socket.on("disconnect", () => {
-    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    console.log("User disconnected!", activeUsers);
-    io.emit("get-users", activeUsers);
-  });
+        // Remove user from the active users 
+        socket.on("disconnect", () => {
+            activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+            console.log("User disconnected!", activeUsers);
+            io.emit('get-users', activeUsers);
+        });
+    
 
-  //send-message: handle sending messages and send the receive message in realtime
-  //only if the other user is active
-  socket.on("send-message", (data) => {
-    const { receiverId } = data;
-    const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending from socket to :", receiverId);
-    console.log("Data", data);
-    if (user) {
-      io.to(user.socketId).emit("receive-message", data);
-    }
-  });
+    // Send Message
+    socket.on("send-message", (data) => {
+        const { receiverId } = data;
+        const user = activeUsers.find((user) => user.userId === receiverId);
+        console.log("Sending from socket to :", receiverId);
+        console.log("Data", data);
+        if (user) {
+            io.to(user.socketId).emit("receive-message", data);
+        }
+    });
+
 });
